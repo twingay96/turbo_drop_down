4e55ec7339119dc4b9f17e575fafe0dd6335b142
twingay96 <flavonoid37@gmail.com>
2023-12-12 23:04:37 +0900

    tailwind.css 추가필요

--- app/javascript/controllers/dropdown_controller.js
+++ app/javascript/controllers/dropdown_controller.js
@@ -2,9 +2,18 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="dropdown"
 export default class extends Controller {
-  static targets = ["dropdownContent","openButton","closeButton"]
+  static targets = ["dropdownContent","openButton","closeButton","active"]
+  static values = {open: Boolean}
+  // open: Boolean은 open이라는 이름의 값을 정의하고, 이 값이 불리언(Boolean) 타입이 될 것임을 나타냅니다. 
+
   connect(){
-    this.closeDropdown()
+    // 처음방문 시에는 열린상태로 
+    if(this.openValue){
+      this.openDropdown()
+    } else{
+      this.closeDropdown()
+    }
+    
   }
   toggleDropdown(){
     if(this.dropdownContentTarget.hidden == true){
@@ -22,9 +31,9 @@ export default class extends Controller {
     } catch{
       // try {} 내부 코드실행중 오류발생시 catch{} 내부코드 실행
     }
-
-    
-    
+    try{
+      this.activeTarget.classList.add("bg-zinc-400")
+    } catch{}
   }
   closeDropdown(){
     this.dropdownContentTarget.hidden = true
@@ -32,7 +41,9 @@ export default class extends Controller {
       this.openButtonTarget.hidden = false
       this.closeButtonTarget.hidden= true
     } catch{}
- 
+    try {
+      this.activeTarget.classList.remove("bg-zinc-400")
+    } catch{}
 
   }
 
