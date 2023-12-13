6edd47f81e7e9972144f562af8e8c8a57fae6f85
twingay96 <flavonoid37@gmail.com>
2023-12-13 23:19:07 +0900

    hover만수정

--- app/javascript/controllers/dropdown_controller.js
+++ app/javascript/controllers/dropdown_controller.js
@@ -1,38 +1,36 @@
 import { Controller } from "@hotwired/stimulus"
-
 // Connects to data-controller="dropdown"
 export default class extends Controller {
   static targets = ["dropdownContent"]
   static values = {open: Boolean}
   // open: Boolean은 open이라는 이름의 값을 정의하고, 이 값이 불리언(Boolean) 타입이 될 것임을 나타냅니다. 
-
   connect(){
-    // 처음방문 시에는 열린상태로 
-    if(this.openValue){
-      this.openDropdown()
-    } else{
-      this.closeDropdown()
-    }
-    
+    // 처음방문 시에는 열린상태로
+    // this.dropdownContentTarget.classList.add('hidden');
+    // if(this.openValue){
+    //   this.openDropdown()
+    // } else{
+    //   this.closeDropdown()
+    // }
   }
-  toggleDropdown(){
-    if(this.dropdownContentTarget.hidden == true){
-      this.openDropdown()
-    } else {
-      this.closeDropdown()
-    }
-    
+  mouseOver() {
+    this.dropdownContentTarget.classList.remove('hidden');
   }
-  openDropdown(){
-
-    this.dropdownContentTarget.hidden = false
-
+  mouseOut() {
+    this.dropdownContentTarget.classList.add('hidden');
   }
-  closeDropdown(){
-    this.dropdownContentTarget.hidden = true
+  // toggleDropdown(){
+  //   if(this.dropdownContentTarget.hidden == true){
+  //     this.openDropdown()
+  //   } else {
+  //     this.closeDropdown()
+  //   }
+  // }
+  // openDropdown(){
+  //   this.dropdownContentTarget.hidden = false
+  // }
+  // closeDropdown(){
+  //   this.dropdownContentTarget.hidden = true
 
-  }
-
-  
-  // connect()는 stimulus 내장함수
+  // }
 }
